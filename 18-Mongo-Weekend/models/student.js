7ed4dcc2cfdcd7f06c31aed092a91f854890c2a8
ronco1812 <89574940+ronco1812@users.noreply.github.com>
2021-11-11 17:59:20 +0200

    mongo

---
+++ 18-Mongo-Weekend/models/student.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
+
+const StudentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surName: {
+    type: String,
+    required: true,
+  },
+  birth: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    unique: true,
+  },
+  gender: {
+    type: String,
+  },
+  courses: [{ type: String }],
+});
+
+const Student = mongoose.model("Student", StudentSchema);
+module.exports = Student;
